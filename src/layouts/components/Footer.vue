355732177ec1189ff145eb1d7ec3a4fcb7349770
Ntiharirizwa Fleury Brillant <ntifleurbrillant@gmail.com>
2026-08-03 10:46:33 +0200

    Initial commit: projet Vue Vuetify

---
+++ src/layouts/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="h-100 d-flex align-center justify-md-space-between justify-center">
+    <!-- 👉 Footer: left content -->
+    <span class="d-flex align-center">
+      &copy;
+      {{ new Date().getFullYear() }}
+      Made With
+      <VIcon
+        icon="bx-heart"
+        color="error"
+        size="1.25rem"
+        class="mx-1"
+      />
+      By <a
+        href="https://themeselection.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="text-primary ms-1"
+      >ThemeSelection</a>
+    </span>
+    <!-- 👉 Footer: right content -->
+    <span class="d-md-flex gap-x-4 text-primary d-none">
+      
+      
+      
+      
+    </span>
+  </div>
+</template>
---
+++ src/layouts/components/NavItems.vue
@@ -0,0 +1,96 @@
+<script setup>
+import VerticalNavSectionTitle from '@/@layouts/components/VerticalNavSectionTitle.vue'
+import VerticalNavGroup from '@layouts/components/VerticalNavGroup.vue'
+import VerticalNavLink from '@layouts/components/VerticalNavLink.vue'
+</script>
+
+<template>
+  <!-- 👉 Dashboards -->
+  <VerticalNavGroup
+    :item="{
+      title: 'Dashboards',
+      badgeContent: '5',
+      badgeClass: 'bg-error',
+      icon: 'bx-home-smile',
+    }"
+  >
+    <VerticalNavLink
+      :item="{
+        title: 'Analytics',
+        to: '/dashboard',
+      }"
+    />
+   
+   
+   
+  </VerticalNavGroup>
+
+ 
+
+  <!-- 👉 Apps & Pagessss -->
+  <VerticalNavSectionTitle
+    :item="{
+      heading: 'Apps & Pages',
+    }"
+  />
+  
+  
+  
+ 
+  <VerticalNavLink
+    :item="{
+      title: 'Login',
+      icon: 'bx-log-in',
+      to: '/login',
+    }"
+  />
+  <VerticalNavLink
+    :item="{
+      title: 'Register',
+      icon: 'bx-user-plus',
+      to: '/register',
+    }"
+  />
+  <VerticalNavLink
+    :item="{
+      title: 'Error',
+      icon: 'bx-info-circle',
+      to: '/no-existence',
+    }"
+  />
+
+  <!-- 👉 User Interface -->
+ 
+  
+  
+ 
+
+  <!-- 👉 Forms & Tables -->
+  <VerticalNavSectionTitle
+    :item="{
+      heading: 'Forms & Tables',
+    }"
+  />
+  <VerticalNavLink
+    :item="{
+      title: 'Form Layouts',
+      icon: 'bx-layout',
+      to: '/form-layouts',
+    }"
+  />
+  
+  
+  <VerticalNavLink
+    :item="{
+      title: 'Tables',
+      icon: 'bx-table',
+      to: '/tables',
+    }"
+  />
+
+  <!-- 👉 Others -->
+ 
+  
+  
+ 
+</template>
